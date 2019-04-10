2e2fdadbfd67471dbfd853e1526e8fc9ed03e40c
othmhiri <49489465+othmhiri@users.noreply.github.com>
2019-04-10 17:22:28 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,8 +7,8 @@ client.on('ready', () => {
 });
 
 client.on('ready', async() => {
-var server = "501332480037748737"; // ايدي السررفر
-var channel = "501332480037748739";//ايدي الروم
+var server = "565552368712810506"; // ايدي السررفر
+var channel = "565552368712810508";//ايدي الروم
     setInterval(()=>{
     client.guilds.get(server).channels.get(channel).send('**Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , **')
     },305);
